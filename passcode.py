4768716ea18140281be50d0da25db0197ad50ea5
mercyakuku <mercyakuku24@gmail.com>
2022-04-25 18:40:40 +0300

    Addeed method that defines properties of a user and saves a new user instance

--- passcode.py
+++ passcode.py
@@ -10,3 +10,14 @@ class User:
     user_list = []
 
     def __init__(self, username, password):
+        """
+        method that defines the properties of a user.
+        """
+        self.username = username
+        self.password = password
+
+    def save_user(self):
+        """
+        A method that saves a new user instace into the user list
+        """
+        User.user_list.append(self)
