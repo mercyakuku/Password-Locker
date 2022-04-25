9479ffaea28ce64720c00538bf44b534a17ec470
mercyakuku <mercyakuku24@gmail.com>
2022-04-25 20:11:06 +0300

    Added functions to create and save a new user

--- interface.py
+++ interface.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3.6
 from passcode  import User, Credentials
+
+
+def create_new_user(username,password):
+    '''
+    Function to create a new user with a username and password
+    '''
+    new_user = User(username,password)
+    return new_user
+
+def save_user(user):
+    '''
+    Function to save a new user
+    '''
+    user.save_user()
