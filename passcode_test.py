07d70be07ef9973458e3034d85522f5377b735fc
mercyakuku <mercyakuku24@gmail.com>
2022-04-25 21:56:59 +0300

    Created a TestClass

--- passcode_test.py
+++ passcode_test.py
@@ -1,3 +1,28 @@
 import unittest
 from passcode import User
 from passcode import Credentials
+
+class TestClass(unittest.TestCase):
+    """
+    A Test class that defines test cases for the User class.
+    """
+    def setUp(self):
+        """
+        Method that runs before each individual test methods run.
+        """
+        self.new_user = User('OwitiCharles','XyZ3thf1')
+
+    def test_init(self):
+        """
+        test case to chek if the object has been initialized correctly
+        """
+        self.assertEqual(self.new_user.username,'OwitiCharles')
+        self.assertEqual(self.new_user.password,'XyZ3thf1')
+
+    def test_save_user(self):
+        """
+        test case to test if a new user instance has been saved into the User list
+
+        """
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
