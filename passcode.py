619d007ac2866ba2cb61ef1bbdd90f55c06af8c9
mercyakuku <mercyakuku24@gmail.com>
2022-04-25 18:25:38 +0300

    Addeed user class

--- passcode.py
+++ passcode.py
@@ -1,2 +1,12 @@
 import random
 import string
+
+
+class User:
+    """
+    Create User class that generates new instances of a user.
+
+    """
+    user_list = []
+
+    def __init__(self, username, password):
