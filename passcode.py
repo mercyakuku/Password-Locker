0b5f2246b1037dfc9a713647caa33d3d0130f653
mercyakuku <mercyakuku24@gmail.com>
2022-04-26 08:34:44 +0300

    Updated passcode.py file

--- passcode.py
+++ passcode.py
@@ -3,46 +3,44 @@ import string
 import pyperclip
 
 class User:
-    """
-    Create User class that generates new instances of a user.
+    
+    # Created a User class that generates new instances of a user.
 
-    """
     user_list = []
 
     def __init__(self, username, password):
-        """
-        method that defines the properties of a user.
-        """
+        
+        # Method that defines the properties of a user.
+
         self.username = username
         self.password = password
 
     def save_user(self):
-        """
-        A method that saves a new user instace into the user list
-        """
+        
+        # A method that saves a new user instance into the user list
+        
         User.user_list.append(self)
     
-
     @classmethod
     def display_user(cls):
         return cls.user_list
 
     def delete_user(self):
-        '''
-        delete_account method deletes a  saved account from the list
-        '''
+        
+        # Method deletes a  saved account from the list
+        
         User.user_list.remove(self)
 
 class Credentials():
-    """
-    Created credentials class to help create new objects of credentials
-    """
+    
+    # Created credentials class to help create new objects of credentials
+    
     credentials_list = []
     @classmethod
     def verify_user(cls,username, password):
-        """
-        method to verify whether the user is in our user_list or not
-        """
+        
+        # Method to verify whether the user is in the user_list or not
+    
         a_user = ""
         for user in User.user_list:
             if(user.username == username and user.password == password):
@@ -50,31 +48,30 @@ class Credentials():
         return a_user
 
     def __init__(self,account,userName, password):
-        """
-        method that defines user credentials to be stored
-        """
+        
+        # Method that defines user credentials to be stored
+        
         self.account = account
         self.userName = userName
         self.password = password
     
     def save_details(self):
-        """
-        method to store a new credential to the credentials list
-        """
+        
+        # Method to store a new credential to the credentials list
+        
         Credentials.credentials_list.append(self)
 
     def delete_credentials(self):
-        """
-        delete_credentials method that deletes an account credentials from the credentials_list
-        """
+        
+        # Method that deletes an account credentials from the credentials_list
+        
         Credentials.credentials_list.remove(self)
     
     @classmethod
     def find_credential(cls, account):
-        """
-        Method that takes in a account_name and returns a credential that matches that account_name.
+        
+        # Method that takes in an account_name and returns a credential that matches that account_name.
 
-        """
         for credential in cls.credentials_list:
             if credential.account == account:
                 return credential
@@ -85,22 +82,23 @@ class Credentials():
 
     @classmethod
     def if_credential_exist(cls, account):
-        """
-        Method that checks if a credential exists from the credential list and returns true or false depending if the credential exists.
-        """
+        
+        # Method that checks if a credential exists from the credential list and returns true or false  if the credential exists or not.
+        
         for credential in cls.credentials_list:
             if credential.account == account:
                 return True
         return False
     @classmethod
     def display_credentials(cls):
-        """
-        Method that returns all items in the credentials list
+        
+        # Method that returns all accounts in the credentials list
 
-        """
         return cls.credentials_list
 
-    def generatePassword(stringLength=8):
-        """Generate a random password string of letters and digits and special characters"""
-        password = string.ascii_uppercase + string.ascii_lowercase + string.digits + "~!@#$%^&*"
+    def generatePassword(stringLength=10):
+
+        # Generate a random password string of letters, digits and special characters
+
+        password = string.ascii_uppercase + string.ascii_lowercase + string.digits + "~(!@#$%^&*)"
         return ''.join(random.choice(password) for i in range(stringLength))
